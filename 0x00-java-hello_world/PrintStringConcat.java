b441fd227a3d47a579ee1c543164972fa7c54b12
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 18:04:28 -0300

    Task 04

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,7 +1,7 @@
 public class PrintStringConcat {
     public static void main(String[] args) {
         String texto1 = "Holberton";
-        String texto2 = "School!";
+        String texto2 = "School";
 
         System.out.println("Bem vindo a " + texto1 + ' ' + texto2 + "!");
     }
