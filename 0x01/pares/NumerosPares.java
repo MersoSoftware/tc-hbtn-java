51517776fb7e42ff2bde5ae6b3d1a3a2d0ff8d81
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 20:13:30 -0300

    Task 001

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,17 @@
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        int num =100;
+        for(int i = 0; i<num; i++){
+
+            if(i %2 == 0){
+                System.out.print(i+", ");
+            if(i == 98){
+                System.out.println("");
+            }
+            }
+
+        }
+
+    }
+}
