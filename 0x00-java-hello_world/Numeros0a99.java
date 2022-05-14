e2b37cdb97ba06bacf726a9440b7b2a2abb61527
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 18:44:08 -0300

    Task 00

---
+++ 0x00-java-hello_world/Numeros0a99.java
@@ -0,0 +1,12 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        int num = 99;
+        for(int i =1; i <= num ; i++){
+            System.out.print(i+", ");
+            if(i == 98){
+                System.out.println(i+1);
+            }
+        }
+
+    }
+}
