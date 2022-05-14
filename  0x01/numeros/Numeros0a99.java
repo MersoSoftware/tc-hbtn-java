cd04f091821fa995eeb3494de25d1887664ebe94
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 18:54:59 -0300

    Task 00

---  0x01/numeros/Numeros0a99.java
+++  0x01/numeros/Numeros0a99.java
@@ -1,7 +1,7 @@
 public class Numeros0a99 {
     public static void main(String[] args) {
         int num = 99;
-        for(int i =1; i <= num ; i++){
+        for(int i =0; i <= num ; i++){
             System.out.print(i+", ");
             if(i == 98){
                 System.out.println(i+1);
