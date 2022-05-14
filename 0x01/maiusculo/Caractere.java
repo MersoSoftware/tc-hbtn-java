21a0742916b3b853b6c946ffebedd0ac7332365e
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 20:42:52 -0300

    Task 02

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -4,12 +4,7 @@ public class Caractere {
 
 
     public static boolean EhMaiusculo(char c){
-
-        if(Character.isUpperCase(c)){
-            return true;
-        }else{
-            return false;
-        }
+        return Character.isUpperCase(c);
     }
 
 }
