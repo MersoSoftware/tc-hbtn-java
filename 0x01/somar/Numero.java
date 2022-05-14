f8facce2338c8975e820482ce2dbc79e33788487
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 20:58:43 -0300

    Task 03

--- 0x01/somar/Numero.java
+++ 0x01/somar/Numero.java
@@ -1,13 +1,6 @@
 package somar;
 
 public class Numero {
-    public static void main(String[] args) {
-
-        System.out.printf("%d\n", Numero.somar(24, 32));
-        System.out.printf("%d\n", Numero.somar(5, 0));
-        System.out.printf("%d\n", Numero.somar(4, -2));
-    }
-
     public static Integer somar(int a, int b) {
         int resultado = a + b;
         return resultado;
