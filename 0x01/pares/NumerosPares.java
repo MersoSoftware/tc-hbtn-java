8f69876047459acc9938b4c9800f498d22ca59ef
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 20:17:24 -0300

    Task 01

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -2,13 +2,14 @@
 public class NumerosPares {
     public static void main(String[] args) {
         int num =100;
-        for(int i = 0; i<num; i++){
+        for(int i = 0; i<num-2; i++){
 
             if(i %2 == 0){
                 System.out.print(i+", ");
-            if(i == 98){
-                System.out.println("");
+
             }
+            if(i == 96){
+                System.out.println(i+2);
             }
 
         }
