e02c6da977e5810d02796f790b24218867e7faaf
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-15 12:19:51 -0300

    Task 00

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,15 +1,13 @@
 
 public class Numero {
-    static int resultado;
+    static int resultado = 0;
     public static void dividir(int a, int b){
         try {
            resultado =  a / b;
         }catch (ArithmeticException e){
-            System.out.println(" Nao eh possivel dividir por zero ");
+            System.out.println("Nao eh possivel dividir por zero");
         } finally {
-
             System.out.println(a + " / " + b + " = " + resultado);
-
         }
 
     }
