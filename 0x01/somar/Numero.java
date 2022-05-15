d717df79515c2a1c9a1d062eb5a5137c3ef859ff
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-14 21:00:02 -0300

    Task 03

--- 0x01/somar/Numero.java
+++ 0x01/somar/Numero.java
@@ -1,7 +1,7 @@
 package somar;
 
 public class Numero {
-    public static Integer somar(int a, int b) {
+    public static int somar(int a, int b) {
         int resultado = a + b;
         return resultado;
     }
