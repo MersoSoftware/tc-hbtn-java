930716493b05bad6f92abe317090ba269897a9ef
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-16 19:24:15 -0300

    0x03-Task-00

---
+++ 0x03/salario/Departamento.java
@@ -0,0 +1,28 @@
+
+public class Departamento {
+
+    double valorMeta;
+    double valorAtingidoMeta;
+
+    public Departamento(double valorMeta, double valorAtingidoMeta) {
+        this.valorMeta = valorMeta;
+        this.valorAtingidoMeta = valorAtingidoMeta;
+    }
+
+    public double getValorMeta() {
+        return valorMeta;
+    }
+
+    public double getValorAtingidoMeta() {
+        return valorAtingidoMeta;
+    }
+
+    public boolean alcancouMeta(){
+        if(this.valorAtingidoMeta >= this.valorMeta){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
