5e483493ca3416fb06db1aeede977bb9713c33ab
Emerson de Abreu Neves <CIANDT\emersonneves@lnb035844cps.ciandt.global>
2022-05-17 22:25:00 -0300

    Task 03

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,48 @@
+public class PersonagemGame {
+
+    public int saudeAtual;
+    public String nome;
+
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        if(saudeAtual > 0){
+            status = "vivo"
+        }
+        if(saudeAtual == 0){
+            status = "morto"
+        }
+
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void tomarDano (int quantidadeDeDano){
+        this.saudeAtual-= quantidadeDeDano;
+        if(this.saudeAtual <= 0){
+            this.setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        this.saudeAtual += quantidadeDeCura;
+        if(this.saudeAtual >= 100){
+            this.setSaudeAtual(100);
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
