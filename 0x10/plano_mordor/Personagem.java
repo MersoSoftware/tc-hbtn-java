3908c5150857d2349d1fb74d80f63343d6e6f954
Emrson Abreu <mersosoftware@gmail.com>
2022-05-30 11:13:56 -0300

    Task-00

--- 0x10/plano_mordor/Personagem.java
+++ 0x10/plano_mordor/Personagem.java
@@ -1,31 +1,25 @@
 
 public class Personagem {
-    protected int pontosDeFelicidade;
-    public Humor obterHumorAtual() {
-        Humor humor;
-        int pontosF = this.pontosDeFelicidade;
-        if (pontosF < -5) {
-            humor = new Irritado();
-        } else if (pontosF > -5 && pontosF < 0) {
-            humor = new Triste();
-        } else if (pontosF >= 1 && pontosF <= 15) {
-            humor = new Feliz();
-        } else {
-            humor = new MuitoFeliz();
-        }
-
-        return humor;
+    protected int pontosDeFelicidade =0;
+    public Humor obeterHumorAtual(){
+        if (this.pontosDeFelicidade < -5)
+            return new Irritado();
+        else if (this.pontosDeFelicidade >= -5 && this.pontosDeFelicidade <= 0)
+            return new Triste();
+        else if (this.pontosDeFelicidade >= 1 && this.pontosDeFelicidade <= 15)
+            return new Feliz();
+        else
+            return new MuitoFeliz();
     }
 
-    public void comer(Comida[] comidas) {
-        for (Comida comida : comidas) {
+    public void comer (Comida[] comidas){
+        for (Comida comida: comidas) {
             this.pontosDeFelicidade += comida.getPontosDeFelicidade();
         }
     }
 
     @Override
     public String toString() {
-        String humor = obterHumorAtual().getClass().getSimpleName();
-        return this.pontosDeFelicidade + " - " + humor;
+        return this.pontosDeFelicidade + " - " + obeterHumorAtual().getClass().getSimpleName();
     }
 }
