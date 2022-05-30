73561b9f6d01fb605b0e8215ab87e4edcac610ca
Emrson Abreu <mersosoftware@gmail.com>
2022-05-30 15:08:21 -0300

    Warappers

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -2,16 +2,17 @@ public class InteiroPositivo {
     private int valor;
 
     public InteiroPositivo(String valor) {
-        this.valor = Integer.parseInt(valor);
+        int value = Integer.parseInt(valor);
+
+        if (value < 0){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+        this.valor = value;
     }
 
     public InteiroPositivo(int valor) {
         this.valor = Integer.parseInt(String.valueOf(valor));
     }
-
-//    public void valida() throws Exception {
-//
-//    }
 
     public boolean ehPrimo(){
         if(valor <= 1){
@@ -27,7 +28,7 @@ public class InteiroPositivo {
 
     public void setValor(int valor) {
         if (valor <  1  ){
-            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+            throw new NumberFormatException("Valor nao eh um valor inteiro positivo");
         }
         this.valor = valor;
     }
