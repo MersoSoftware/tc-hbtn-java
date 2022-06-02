122c18a988286eb99989f20322f0739684dd8233
Emrson Abreu <mersosoftware@gmail.com>
2022-06-02 09:12:29 -0300

    Task-00

--- Enum/celular/Celular.java
+++ Enum/celular/Celular.java
@@ -1,56 +1,46 @@
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Celular {
-    private ArrayList<Contato> contatos = new ArrayList<Contato>();
+    ArrayList <Contato> contatos;
 
-    public int obterPosicaoContato(String nomeContato) {
-        for (Contato contato : this.contatos) {
-            if (contato.getNome().equals(nomeContato)) {
-                return this.contatos.indexOf(contato);
+    public Celular(){
+        this.contatos = new ArrayList<Contato>();
+    }
+    public int obterPosicaoContato(String nome) {
+        for (int posicaoContato = 0; posicaoContato < this.contatos.size(); posicaoContato++) {
+            if (contatos.get(posicaoContato).getNome().equals(nome)) {
+                return posicaoContato;
             }
         }
         return -1;
     }
-
-    public void adicionarContato(Contato contato) {
-        int posicaoContato = obterPosicaoContato(contato.getNome());
-
-        if (posicaoContato != -1) {
+    public void adicionarContato(Contato contato){
+        if(obterPosicaoContato(contato.getNome()) != -1)
             throw new IllegalArgumentException("Nao foi possivel adicionar contato. Contato jah existente com esse nome");
-        }
-
         this.contatos.add(contato);
     }
+    public void atualizarContato(Contato contatoAntigo, Contato novoContato){
+        int posicaoAntigo = obterPosicaoContato(contatoAntigo.getNome());
 
-    public void atualizarContato(Contato contatoAntigo, Contato novoContato) {
-        int posicaoContatoAntigo = obterPosicaoContato(contatoAntigo.getNome());
-        int posicaoNovoContato = obterPosicaoContato(novoContato.getNome());
-
-        if (posicaoContatoAntigo == -1) {
+        if (posicaoAntigo != -1){
+            if (! contatoAntigo.getNome().equals(novoContato.getNome()))
+                throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato jah existente com esse nome");
+            this.contatos.set(posicaoAntigo, novoContato);
+        }else
             throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato nao existe");
-        }
 
-        if (posicaoNovoContato != -1 && posicaoNovoContato != posicaoContatoAntigo) {
-            throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato jah existente com esse nome");
-        }
-
-        this.contatos.set(posicaoContatoAntigo, novoContato);
     }
 
-    public void removerContato(Contato contato) {
+    public void removerContato(Contato contato){
         int posicaoContato = obterPosicaoContato(contato.getNome());
 
-        if (posicaoContato == -1) {
+        if (posicaoContato == -1)
             throw new IllegalArgumentException("Nao foi possivel remover contato. Contato nao existe");
-        }
-
-        this.contatos.removeIf(contatoIt -> contatoIt.getNome().equals(contato.getNome()));
+        this.contatos.remove(posicaoContato);
     }
+    public void listarContatos(){
+        for (Contato contato : contatos){
+            System.out.println(contato.getNome() + " -> " + contato.getNumero() + " (" + contato.getTipo() + ")");
 
-    public void listarContatos() {
-        for (Contato contato : this.contatos) {
-            System.out.println(contato.getNome() + " -> " + contato.getNumero() + " (" + contato.getTipoNumero() + ")");
         }
     }
 }
