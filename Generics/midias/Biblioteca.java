f0c3eeaaa268074f772451de55510eed225c0fb3
Emrson Abreu <mersosoftware@gmail.com>
2022-06-09 08:50:33 -0300

    Task-00

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,12 @@
+public class Biblioteca <T extends Midia>{
+    private List<T> midias = new ArrayList<>();
+
+    public void adicionarMidia(T t){
+        this.midias.add(t);
+    }
+
+    public List<T> obterListaMidias(){
+        return midias;
+    }
+
+}
